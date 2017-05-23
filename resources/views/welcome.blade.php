155e419ecf436480faa31be5fbbfe0b0a0aead3a
Dominik Mihelj <dmihelj@tvz.hr>
2017-05-23 15:25:20 +0200

    Promijenjen welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Luanavel
                 </div>
 
                 <div class="links">
