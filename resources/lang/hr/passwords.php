9e3dadf1a7274b74ab19f762e3bb6101de377f5b
vagrant <vagrant@homestead>
2017-08-25 16:05:14 +0000

    Dodan locale

---
+++ resources/lang/hr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Lozinka mora imati barem 6 znakova i odgovarati ponovljenoj lozinki.',
+    'reset' => 'Lozinka je resetirana!',
+    'sent' => 'Na Vašu mail adresu poslan je link za reset lozinke!',
+    'token' => 'Token za reset lozinke nije ispravan.',
+    'user' => "Ne postoji korisnik s tom e-mail adresom.",
+
+];
